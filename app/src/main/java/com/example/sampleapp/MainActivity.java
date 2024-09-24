02a55f1b8460c2c01df15a9b001cc418aa055c09
usridas <umaiyal.sridas@gmail.com>
2024-09-24 11:12:47 -0700

    start project

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sampleapp;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setVerticalScrollBarEnabled(true);
+        myWebView.loadUrl("https://usridas.github.io/sampleapp/");
+    }
+}
